2af09544a5f504e9a2394c483554553ae4dc9b47
Chris Trotman <solarnz@teameos.org>
2012-04-29 10:03:36 +1000

    Rename to toroplus (1/2)

--- full_toroplus.mk
+++ full_toroplus.mk
@@ -28,8 +28,8 @@ PRODUCT_PACKAGES := \
 
 #if we do this after the full_base_telephony is included some of these don't get picked up..
 PRODUCT_COPY_FILES += \
-    device/samsung/torospr/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
-    device/samsung/torospr/apns-conf.xml:system/etc/apns-conf.xml \
+    device/samsung/toroplus/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
+    device/samsung/toroplus/apns-conf.xml:system/etc/apns-conf.xml \
     frameworks/base/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml
 
 # Inherit from those products. Most specific first.
@@ -37,11 +37,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # This is where we'd set a backup provider if we had one
 #$(call inherit-product, device/sample/products/backup_overlay.mk)
 # Inherit from toro device
-$(call inherit-product, device/samsung/torospr/device.mk)
+$(call inherit-product, device/samsung/toroplus/device.mk)
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := full_torospr
-PRODUCT_DEVICE := torospr
+PRODUCT_NAME := full_toroplus
+PRODUCT_DEVICE := toroplus
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := Galaxy Nexus
 
